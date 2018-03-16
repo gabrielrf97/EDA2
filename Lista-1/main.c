d7c3249c752cc9ed8607ce0343b2f8e7137474b5
Gabriel Albino <enggabrielalbino@gmail.com>
2018-03-16 17:06:24 -0300

    Added method to find a place to fit desired number

--- Lista-1/main.c
+++ Lista-1/main.c
@@ -7,30 +7,37 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
-void populate_data_vector(int* vector, int size, float securityMargin, int* emptyPositionTracker);
-void populate_second_index_arr(int* index2, int* dataArray);
-void populate_first_index_arr(int* index1, int* index2);
-void create_initial_menu();
+#define size_index_1 10
+#define size_index_2 100
+#define initialVectorSize 10000
+
+int* populate_data_vector(int size, float securityMargin, int* emptyPositionTracker);
+int* populate_second_index_arr(int* dataArray);
+int* populate_first_index_arr(int* index2);
+void create_initial_menu(int* index1, int* index2, int* dataArray);
+int find_slot_for_value(int* index1, int* index2, int* dataArray, int number);
 
 int main(int argc, const char * argv[]) {
     
-    int* dataArray, index2, index1, emptyPositionTracker;
-    int initialVectorSize = 10000;
-    float securityMargin = 1.1;
+    int *dataArray, *index2, *index1, *emptyPositionTracker  = calloc( 100 , sizeof(int));
+    float securityMargin = 1.0;
     
     //Checka esses & ai
-    populate_data_vector(&dataArray, initialVectorSize, securityMargin, &emptyPositionTracker);
-    populate_second_index_arr(&index2, &dataArray);
-    populate_first_index_arr(&index1, &index2);
+    dataArray = populate_data_vector(initialVectorSize, securityMargin, emptyPositionTracker);
+    index2 = populate_second_index_arr(dataArray);
+    index1 = populate_first_index_arr(index2);
     
-    create_initial_menu();
+    create_initial_menu(index1, index2, dataArray);
 
     return 0;
 }
 
-void create_initial_menu() {
-    //do{
+void create_initial_menu(int* index1, int* index2, int* dataArray){
+    char userInput;
+    int value;
+    do{
         printf("*****************************\n");
         printf("* Escolha sua ação:         *\n");
         printf("******************+**********\n");
@@ -40,72 +47,124 @@ void create_initial_menu() {
         printf("* 4 - Itens vazios por slot *\n");
         printf("* 5 - Encerrar              *\n");
         printf("*****************************\n");
-        //char userInput = getc()
-//    switch (userInput) {
-//        case 1:
-    //            chama o alg de busca (primeiro, pesquisa no primeiro index:
-                    //algo do tipo (for, de 0 até tamanho do index 1 - 1)
-                        //se num < index1[i]
-                            //busca no index2, das posicoes i*10 até (1*10) + 10
-                                //achou o slot do numero? ve se tem vaga, consultando a posição
-                                //do emptyPositionTracker
-                                //Tem vaga, da shift em todos os elementos e insere na posicao que ele tem que ficar
-//            break;
-//        case 2:
+        do{
+            userInput = getchar();
+        }while(userInput == '\n');
+        switch (userInput) {
+        case '1':
+                printf("Insira o valor que deseja inserir: ");
+                scanf("%d", &value);
+                find_slot_for_value(index1, index2, dataArray, value);  
+            break;
+//        case '2':
 //                 mesma coisa do alg anterior, so que da shiftback, e tem que ver se o numero existe ou remover pelo index dele( a segunda é mais facil)
 //            break;
-//        case 3:
+//        case '3':
 //                 parecido com o case 1, so que sem validar se tem espaço e sem dar shift, obviamente
 //            break;
+//        case '4':
                     //printa o emptyPositionTracker
-//        case 5:
-    //               return 0;
-//            break;
-//        default:
-                //printa (usuario burro)
-//            break;
-//    }
-    //}while(userInput != 4)
+        case '5':
+            //do nothing
+            break;
+        default:
+            printf ("wtf dude?\n\n");
+        }
+    }while(userInput != '5');
 }
 
 //Criar vetor aleatorio, com 11000 posições, sendo que das 11000, só 10000 terão valores
-void populate_data_vector(int* vector, int size, float securityMargin, int* emptyPositionTracker){
+int* populate_data_vector(int size, float securityMargin, int* emptyPositionTracker){
     
     int fullSize = (int) size * securityMargin;
-    vector = calloc( fullSize , sizeof(int));
-    
+    int *vector = calloc( fullSize , sizeof(int));            
     //Populating the 10000 first positions
     for (int i = 0; i < fullSize ; i++){
         //Left a black space in vector
-        if (i % 100 == 0)
+        if (i % 90 == 0 && i + 10 <= fullSize)
             i += 10;
-        vector[i] = i * 5;
+        if(i < fullSize)
+            vector[i] = i * 5;
+       
     }
     
     //Populate Empty position tracker
     for (int i = 0; i < 100; i++) {
         emptyPositionTracker[i] = 10;
     }
+
+    return vector;
 }
 
 //Criar index secundário, que vai apontar para o vetor original/pula de 100 em 100
-void populate_second_index_arr(int* index2, int* dataArray){
-    
-    int size_index_2 = 100;
-    
-    for(int i = 0; i < size_index_2; i++){
+int* populate_second_index_arr(int* dataArray){
+    int *index2 = calloc( size_index_2 , sizeof(int));
+    for(int i = 0; i < size_index_2; i++){   
         index2[i] = dataArray[i * 100];
     }
+    return index2;
 }
 
 //Criar index primário, que aponta para o vetor secundario, pula de 10 em 10
-void populate_first_index_arr(int* index1, int* index2) {
-    int size_index_1 = 10;
-    
+int* populate_first_index_arr(int* index2) {  
+    int *index1 = calloc( size_index_1 , sizeof(int));  
     for(int i = 0; i < size_index_1; i++){
         index1[i] = index2[i * 10];
     }
+    return index1;
 }
 
 
+int find_slot_for_value(int* index1, int* index2, int* dataArray, int number){
+    int get_first_index_position(int* index1, int number);
+    int get_second_index_position(int* index2, int number, int first_index_position);
+    int get_position_in_data_vector(int* dataArray, int number, int second_index_position);
 
+    int first_index_position = get_first_index_position(index1, number);
+    int second_index_position = get_second_index_position(index2, number, first_index_position);
+    int position = get_position_in_data_vector(dataArray, number, second_index_position);
+    //printf("* DEBUG:\n* Valor: %d\n* Anterior: [%d] = %d\n* Próximo: [%d] = %d\n",number, position, dataArray[position], position+1, dataArray[position+1]);
+    return position;
+}
+
+int get_first_index_position(int* index1, int number){
+    int position = -1;
+    for(int i = 0; i < size_index_1-1; i++){
+        if(position == -1 && number < index1[i+1]){
+            position = i;
+        }
+    }
+    if(position == -1){ //Number is higher than others.
+        position = size_index_1-1; // fit it on the last index
+    }
+    return position;
+}
+
+int get_second_index_position(int* index2, int number, int first_index_position){
+    int position = -1;
+    for(int i = first_index_position*10; i < (first_index_position + 1)*10; i++){
+        if(position == -1 && number < index2[i+1]){
+            position = i;
+        }
+    }
+    if(position == -1){
+        position = (first_index_position + 1)*10-1;
+    }
+    return position;
+}
+
+
+int get_position_in_data_vector(int* dataArray, int number, int second_index_position){
+    int startPos = second_index_position*100;
+    int endPos = (second_index_position + 1)*100;
+    int position = startPos;
+    for(int i = startPos; number > dataArray[i] && i < initialVectorSize; i++){
+        if(number < dataArray[i+1] || dataArray[i+1] == 0){
+            position = i;
+        }
+    }
+    while(dataArray[position] == 0 && position > 0){
+        position--;
+    }
+    return position;
+}
